3c6a69af444d85ce3e67d7f80775105b0a4ee9e2
digituno <digituno@gmail.com>
2022-05-16 19:54:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/eshop/dazzle/DazzleApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.dazzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DazzleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DazzleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eshop/dazzle/DazzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.dazzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DazzleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
