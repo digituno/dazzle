c0dbcab1fec3c3762994a737c53707c3329c70fe
digituno <digituno@gmail.com>
2022-05-19 22:11:18 +0900

    thymeleaf 환경 설정

---
+++ src/main/java/com/eshop/dazzle/board/BoardController.java
@@ -0,0 +1,16 @@
+package com.eshop.dazzle.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/board")
+    public String board(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "board/index";
+    }
+}
