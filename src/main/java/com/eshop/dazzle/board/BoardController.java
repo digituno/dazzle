f5a91402b4cde5fe56b9e7afe93afa966f22e932
digituno <digituno@gmail.com>
2022-05-21 17:07:44 +0900

    타임 리프, 부트스트랩이 적용된 웰컴 페이지 생성

--- src/main/java/com/eshop/dazzle/board/BoardController.java
+++ src/main/java/com/eshop/dazzle/board/BoardController.java
@@ -1,16 +1,16 @@
 package com.eshop.dazzle.board;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 
-    @GetMapping("/board")
-    public String board(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "board/index";
+    @GetMapping("/list")
+    public String list() {
+        return "board/list";
     }
 }
+
